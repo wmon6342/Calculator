a594d18c1a3e8d35609babcbb15baadb62d73f22
wmon6342 <montwj10@gmail.com>
2022-11-08 16:49:53 +0000

    Add Keyboard Support

--- jscript.js
+++ jscript.js
@@ -1,34 +1,40 @@
 function add(x) {
     let result = x.reduce((a, b) => a + b);
-    return result;
+    return result.toFixed(2);
 }
 
 function subtract(x) {
     let result = x.reduce((a, b) => a - b);
-    return result;
+    return result.toFixed(2);
 }
 
 function multiply(x) {
     let result = x.reduce((a, b) => a * b);
-    return result;
+    return result.toFixed(2);
 }
 
 function divide(x) {
     let result = x.reduce(function(a, b) {
         if(a && b !== 0) {
             let c = a / b;
-            let d = Math.round(c);
+            let d = c.toFixed(2);
             return d;
         } else {
-            return 'What are u doing u muppet';
+            //we need to make sure that this value cannot be used in a future calculation
+            return 'really';
         }
     });
     return result;
 }
 
 function strToNumber(z) {
-    let result = z.map((item) => +(item))
+    let result = z.map((item) => +(item));
     return result;
+}
+
+function deleteLast(z) {
+    let y = z.slice(0, -1);
+    return y;
 }
 
 function operator(x) {
@@ -85,6 +91,7 @@ const six = document.querySelector('#six');
 const seven = document.querySelector('#seven');
 const eight = document.querySelector('#eight');
 const nine = document.querySelector('#nine');
+const decimal = document.querySelector('#decimal_point');
 
 const plus = document.querySelector('#add');
 const minus = document.querySelector('#subtract');
@@ -94,6 +101,7 @@ const divisor = document.querySelector('#divide');
 const backspace = document.querySelector('#backspace');
 const clear = document.querySelector('#clear');
 
+let buttonClick = 0;
 
 function findButton() {
     Array.from(buttons).filter(function(button) {
@@ -130,26 +138,44 @@ function findButton() {
         if(button === nine) {
             screen.textContent += '9';
         }
+        if(button === decimal) {
+            //insert an if statement checking whether a number has already got a decimal point in it
+            buttonClick ++;
+            if(buttonClick > 1){
+                screen.textContent += '';
+            } else {
+                screen.textContent += '.';
+            }
+        }
         if(button === clear) {
+            buttonClick = 0;
             screen.textContent = '';
+        }
+        if(button === backspace) {
+            screen.textContent = deleteLast(screen.textContent);
         }
         if(button === plus) {
             operator(displayValue);
+            buttonClick = 0;
             screen.textContent += '+';
         }
         if(button === minus) {
             operator(displayValue);
+            buttonClick = 0;
             screen.textContent += '-';
         }
         if(button === times) {
             operator(displayValue);
+            buttonClick = 0;
             screen.textContent += '*';
         }
         if(button === divisor) {
             operator(displayValue);
+            buttonClick = 0;
             screen.textContent += '/';
         }
         if(button === equals) {
+            buttonClick = 0;
             operator(displayValue);
         }
     });
@@ -157,6 +183,84 @@ function findButton() {
 }
 
 findButton();
+
+function keyboardSupport() {
+    screen.textContent = '';
+    let displayValue = [];
+    document.addEventListener('keydown', function(event) {
+        if(event.key === '1') {
+            screen.textContent += '1';
+        }
+        if(event.key === '2') {
+            screen.textContent += '2';
+        }
+        if(event.key === '3') {
+            screen.textContent += '3';
+        }
+        if(event.key === '4') {
+            screen.textContent += '4';
+        }
+        if(event.key === '5') {
+            screen.textContent += '5';
+        }
+        if(event.key === '6') {
+            screen.textContent += '6';
+        }
+        if(event.key === '7') {
+            screen.textContent += '7';
+        }
+        if(event.key === '8') {
+            screen.textContent += '8';
+        }
+        if(event.key === '9') {
+            screen.textContent += '9';
+        }
+        if(event.key === '0') {
+            screen.textContent += '0';
+        }
+        if(event.key === '+') {
+            operator(displayValue);
+            buttonClick = 0;
+            screen.textContent += '+';
+        }
+        if(event.key === '-') {
+            operator(displayValue);
+            buttonClick = 0;
+            screen.textContent += '-';
+        }
+        if(event.key === '*') {
+            operator(displayValue);
+            buttonClick = 0;
+            screen.textContent += '*';
+        }
+        if(event.key === '/') {
+            operator(displayValue);
+            buttonClick = 0;
+            event.preventDefault();
+            screen.textContent += '/';
+        }
+        if(event.key === '.') {
+            buttonClick++;
+            screen.textContent += '.';
+        }
+        if(event.key === 'Backspace') {
+            event.preventDefault();
+            screen.textContent = deleteLast(screen.textContent);
+        }
+        if(event.key === 'Enter') {
+            buttonClick = 0;
+            event.preventDefault();
+            operator(displayValue);
+        }
+        if(event.key === 'Delete') {
+            buttonClick = 0;
+            event.preventDefault();
+            screen.textContent = '';
+        }
+    })
+}
+
+keyboardSupport();
 
 
     
